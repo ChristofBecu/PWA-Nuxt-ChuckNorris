c6138388bf21c182276b8861be466a852ba529ff
ChristofBecu <christof.becu@gmail.com>
2020-09-03 13:49:59 +0200

    initial commit

---
+++ components/ChuckNorrisQuote.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p v-if="$fetchState.pending">
+      ... Chuck Norris is downloading the internet into his brain ...
+    </p>
+    <p v-else-if="$fetchState.error">
+      Chuck Norris has broken the internet, because he already knows every single bit of it by heart
+    </p>
+    <p v-else>
+      {{ quote.value }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch () {
+    this.quote = await fetch(
+      'https://api.chucknorris.io/jokes/random'
+    ).then(res => res.json())
+  },
+  data () {
+    return {
+      quote: null
+    }
+  }
+}
+</script>
